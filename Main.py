5b715d36a123125ad8d458e495fccbabafdd4562
Anthony Lam <99anthonylam@gmail.com>
2020-05-05 23:11:51 -0400

    add readme

--- Main.py
+++ Main.py
@@ -8,14 +8,14 @@ from ultimate_ttt import *
 if __name__ == "__main__":
     game = ultimate_ttt()
     #  COMMENT OUT THE QUICKSTARTS FOR NORMAL GAMEPLAY
-    game.quickStart1()  # for testing purposes, two AI of difficulty 3 play against each other
+    # game.quickStart1()  # for testing purposes, two AI of difficulty 3 play against each other
     # game.quickStart2()  # for testing purposes, first AI has difficulty 2, second AI has difficulty 3
 
     #  EVERYTHING BELOW THIS SHOULD BE UNCOMMENTED FOR NORMAL GAMEPLAY
-    # choice = game.setupGame()  # as defined, 1 is human players only, 2 is one AI, 3 is two AI
-    # if choice == 1:
-    #     game.twoHumanPlay()
-    # elif choice == 2:
-    #     game.oneHumanPlay()
-    # else:
-    #     game.twoAIPlay()
+    choice = game.setupGame()  # as defined, 1 is human players only, 2 is one AI, 3 is two AI
+    if choice == 1:
+        game.twoHumanPlay()
+    elif choice == 2:
+        game.oneHumanPlay()
+    else:
+        game.twoAIPlay()
---
+++ README.md
@@ -0,0 +1,17 @@
+# Ultimate Tic-Tac-Toe 
+
+## Introduction
+For our CS591 Game Theory project, we implemented Minimax with Alpha-beta pruning and Monte Carlo Tree Search to determine the age old question of two player games applied to Ultimate Tic-Tac-Toe: Should you go first or second? 
+
+Helpful game rules and illustrations can be found [here](https://ultimate-t3.herokuapp.com/rules).
+
+## Usage
+
+In order to run and play against our agents, clone the git repo and run the respective drivers located in `./MCTS/driver.py` for MCTS and `./Main.py` for Minimax. Customization and play parameters can be found in their respective file comments.
+
+&nbsp;
+
+
+Have fun!
+
+-Jake, Danny, and Anthony
